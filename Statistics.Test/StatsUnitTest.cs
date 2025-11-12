d7f5731df975287d44a2abc471a5990785536842
Mohanraj Devadoss <mohanraj.devadoss@philips.com>
2025-11-12 14:31:30 +0530

    Update code and tests

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,22 +12,25 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{98.6, 98.2, 97.8, 102.2});
+                new List<float> {98.6F, 98.2F, 97.8F, 102.2F});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(computedStats.average - 99.2) <= epsilon);
-            Assert.True(Math.Abs(computedStats.max - 102.2) <= epsilon);
-            Assert.True(Math.Abs(computedStats.min - 97.8) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 99.2F) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 102.2F) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 97.8F) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<float>());
             // All fields of computedStats (average, max, min) must be
             // Double.NaN (not-a-number), as described in
             // https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
             // Specify the Assert statements here
+            Assert.True(float.IsNaN(computedStats.average));
+            Assert.True(float.IsNaN(computedStats.max));
+            Assert.True(float.IsNaN(computedStats.min));
         }
     }
 }
